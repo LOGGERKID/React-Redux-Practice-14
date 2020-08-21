27666439412be55890f4d673ce33c78957d3ed10
LOGGERKID <revanthms.06@gmail.com>
2020-08-22 01:57:18 +0530

    total Price Calculation
    Signed-off-by: LOGGERKID <revanthms.06@gmail.com>

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,13 @@ const initialState = {
   totalPrice: 4,
 };
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+  };
+
 const reducer = (state = initialState, action) => {
   // This is completely mutating the state
   switch (action.type) {
@@ -20,6 +27,7 @@ const reducer = (state = initialState, action) => {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
         },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -28,6 +36,8 @@ const reducer = (state = initialState, action) => {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
         },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+
       };
 
     default:

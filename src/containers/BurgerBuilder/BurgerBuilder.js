fb66345e036f240754e77b369e5e8f9380f563d8
LOGGERKID <revanthms.06@gmail.com>
2020-08-22 02:02:47 +0530

    Order UI State Updating
    Signed-off-by: LOGGERKID <revanthms.06@gmail.com>

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,7 @@ class BurgerBuilder extends Component {
   state = {
     // ingredients: null,
     // totalPrice: 4,
-    purchasable: false,
+    // purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -52,7 +52,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum >0;
   }
 
   addIngredientHandler = (type) => {
@@ -135,7 +135,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.addIngDispatch}
             ingredientRemoved={this.props.remIngDispatch}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.igs)}
             ordered={this.purchaseHandler}
             price={this.props.totPrice}
           />
